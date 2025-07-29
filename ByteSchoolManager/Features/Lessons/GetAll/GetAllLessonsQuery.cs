d2f40715097676d15842004cfa29eb40d9c24ec9
Kirillfty <kirillhaiduk4@gmail.com>
2025-07-29 21:21:40 +0300

    creaate lesonn dto wit get lessons query

---
+++ ByteSchoolManager/Features/Lessons/GetAll/GetAllLessonsQuery.cs
@@ -0,0 +1,27 @@
+﻿using ByteSchoolManager.Common.Abstractions;
+using ByteSchoolManager.Entities;
+using ByteSchoolManager.Repository;
+using MediatR;
+
+namespace ByteSchoolManager.Features.Lessons.GetAll
+{
+
+    public class GetAllLessonsQuery : IQuery<List<LessonDto>>;
+
+
+
+    public class GetAllLessonsQueryHandler : IRequestHandler<GetAllLessonsQuery, List<LessonDto>>
+    {
+        private readonly RepositoryBase<Lesson> _repository;
+
+        public GetAllLessonsQueryHandler(RepositoryBase<Lesson> rep) { 
+            _repository = rep;
+        }
+        public async Task<List<LessonDto>> Handle(GetAllLessonsQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.ListSelectionAsync(
+                x => new LessonDto(x.Id,x.CoachId,x.DateAndTime,x.CoachId,x.Students)
+                );
+        }
+    }
+}
