80df49001d6ae95199dcbf989c5a75079cbd8e52
Kirillfty <kirillhaiduk4@gmail.com>
2025-08-03 12:19:51 +0300

    Reapply "creaate lesonn dto wit get lessons query"
    This reverts commit fac3ebf64877f0cd2e83caacbb620c726d0dfb8e.

---
+++ ByteSchoolManager/Features/Lessons/GetAll/LessonDto.cs
@@ -0,0 +1,20 @@
+﻿using ByteSchoolManager.Entities;
+
+namespace ByteSchoolManager.Features.Lessons.GetAll
+{
+    public record LessonDto(
+        int Id,
+
+        int CourseId,
+
+        DateTime DateAndTime,
+
+        int CoachId,
+
+        int Studentss
+    );
+}
+
+
+
+
