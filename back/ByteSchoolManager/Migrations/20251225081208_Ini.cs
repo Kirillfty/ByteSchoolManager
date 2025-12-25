169a41095d228ff864e88643153a8a4e325a83de
Kirillfty <kirillhaiduk4@gmail.com>
2025-12-25 11:20:04 +0300

    test addStudentCourseRequest

---
+++ back/ByteSchoolManager/Migrations/20251225081208_Ini.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ByteSchoolManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ini : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "StudentLessons",
+                newName: "LessonStatus");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "StudentCourses",
+                newName: "CourseStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LessonStatus",
+                table: "StudentLessons",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "CourseStatus",
+                table: "StudentCourses",
+                newName: "Status");
+        }
+    }
+}
